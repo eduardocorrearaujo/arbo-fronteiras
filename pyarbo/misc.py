18ac4e7879fdcb2154c58120db7a350dcd5a2436
alexdesiqueira <alex.desiqueira@igdore.org>
2022-12-19 16:05:22 -0300

    Teste - adicionando return_dengue_cases

--- pyarbo/misc.py
+++ pyarbo/misc.py
@@ -30,3 +30,32 @@ def fill_missing_dates(data, format="%Y-%m-%d"):
     data = data.reindex(date_min_max, fill_value=0)
 
     return data
+
+
+def return_dengue_cases(data):
+    """Return dengue cases, classified in notified, probable, and lab confirmed.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        Data .
+
+    Returns
+    -------
+
+
+    Notes
+    -----
+    Expected columns: "dt_sin_pri", "classi_fin", "criterio"
+    """
+    data = data.assign(tipo = "notified")
+
+    classifin_not_5 = (data["classi_fin"] != 5)
+    criterio_is_1 = (data["criterio"] == 1)
+
+    data.loc[:, (classifin_not_5, "tipo")] = "probable"
+
+    dengue_mask = classifin_not_5 & criterio_is_1
+    data[dengue_mask]["tipo"] = "lab_confirmed"
+
+    return data
